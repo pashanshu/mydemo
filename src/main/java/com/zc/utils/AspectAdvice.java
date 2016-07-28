b8b6df1259f0cfb3d03fa5fc37e9c1ba2d5f42db
zc <361393235@qq.com>
2016-07-28 18:25:41 +0800

    15

--- src/main/java/com/zc/utils/AspectAdvice.java
+++ src/main/java/com/zc/utils/AspectAdvice.java
@@ -34,7 +34,7 @@ public class AspectAdvice {
  public void doBefore(JoinPoint jp) {
      System.out.println("===========进入before advice============ \n");
 
-     System.out.print("准备在" + jp.getTarget().getClass() + "对象上用");
+//     System.out.print("准备在" + jp.getTarget().getClass() + "对象上用");
 //     System.out.print(jp.getSignature().getName() + "方法进行对 '");
 //     System.out.print(jp.getArgs()[0] + "'进行删除！\n\n");
 
@@ -62,18 +62,18 @@ public class AspectAdvice {
      // 调用目标方法之前执行的动作
      System.out.println("调用方法之前: 执行！\n");
 
-     // 调用方法的参数
-     Object[] args = pjp.getArgs();
-     // 调用的方法名
-     String method = pjp.getSignature().getName();
-     // 获取目标对象(形如：com.action.admin.LoginAction@1a2467a)
-     Object target = pjp.getTarget();
-    //获取目标对象的类名(形如：com.action.admin.LoginAction)
-   String targetName = pjp.getTarget().getClass().getName();
-     // 执行完方法的返回值：调用proceed()方法，就会触发切入点方法执行
+//     // 调用方法的参数
+//     Object[] args = pjp.getArgs();
+//     // 调用的方法名
+//     String method = pjp.getSignature().getName();
+//     // 获取目标对象(形如：com.action.admin.LoginAction@1a2467a)
+//     Object target = pjp.getTarget();
+//    //获取目标对象的类名(形如：com.action.admin.LoginAction)
+//   String targetName = pjp.getTarget().getClass().getName();
+//     // 执行完方法的返回值：调用proceed()方法，就会触发切入点方法执行
      Object result = pjp.proceed();//result的值就是被拦截方法的返回值
-
-     System.out.println("输出：" + args[0] + ";" + method + ";" + target + ";" + result + "\n");
+//
+//     System.out.println("输出：" + args[0] + ";" + method + ";" + target + ";" + result + "\n");
      System.out.println("调用方法结束：之后执行！\n");
    return result;
  }
