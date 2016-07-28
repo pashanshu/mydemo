97f45d6a7eb9b3a68d4dbc72e8fc7b0f7657e9ab
zc <361393235@qq.com>
2016-07-28 18:44:21 +0800

    15

---
+++ src/main/java/com/zc/dao/MyTestMapper.java
@@ -0,0 +1,22 @@
+package com.zc.dao;
+
+import com.zc.model.MyTest;
+import com.zc.model.MyTestExample;
+import java.util.List;
+import org.apache.ibatis.annotations.Param;
+
+public interface MyTestMapper {
+    int countByExample(MyTestExample example);
+
+    int deleteByExample(MyTestExample example);
+
+    int insert(MyTest record);
+
+    int insertSelective(MyTest record);
+
+    List<MyTest> selectByExample(MyTestExample example);
+
+    int updateByExampleSelective(@Param("record") MyTest record, @Param("example") MyTestExample example);
+
+    int updateByExample(@Param("record") MyTest record, @Param("example") MyTestExample example);
+}
